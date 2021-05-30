a397b6baa31531d8375b4b0db1dfd2d28eac90dc
Kanak Shilledar <kanakshilledar111@gmail.com>
2021-05-30 19:21:10 +0530

    Added notes backend

--- website/models.py
+++ website/models.py
@@ -1,7 +1,7 @@
-from datetime import timezone
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,9 +9,10 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    note = db.relationship('Note')
+    notes = db.relationship('Note')
